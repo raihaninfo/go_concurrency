d411c1920dac0435eb30d02f4fdce95c10d7ffed
raihaninfo <raihanmahmudi35@gmail.com>
2022-02-05 14:42:39 +0600

    go routines

--- goRoutines/main.go
+++ goRoutines/main.go
@@ -7,18 +7,18 @@ import (
 
 func main() {
 	fmt.Println("Main Starts")
-	go hello(1)
-	go hello(2)
-	go hello(3)
-	go hello(4)
-	go hello(5)
+	msg := "hello"
+	go func(m string) {
+		fmt.Println(m)
+	}(msg)
+	msg = "world"
 	fmt.Println("Main End")
 
 	time.Sleep(100 * time.Millisecond)
 }
 
-func hello(grn int) {
-	for i := 0; i < 3; i++ {
-		fmt.Println(grn, ": Hello")
-	}
-}
+// func hello(grn int) {
+// 	for i := 0; i < 3; i++ {
+// 		fmt.Println(grn, ": Hello")
+// 	}
+// }
