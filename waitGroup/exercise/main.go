afe6c87bd6be3ccacec393ae9d6af3869610a7e8
raihaninfo <raihanmahmudi35@gmail.com>
2022-02-05 10:56:58 +0600

    wait group exercise

---
+++ waitGroup/exercise/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"sync"
+)
+
+var urls = []string{
+	"https://google.com",
+	"https://facebook.com",
+	"https://twitter.com",
+	"https://github.com",
+}
+
+var wg sync.WaitGroup
+
+func fetchStatus(w http.ResponseWriter, r *http.Request) {
+	for _, url := range urls {
+		wg.Add(1)
+		go func(url string) {
+			resp, err := http.Get(url)
+			if err != nil {
+				fmt.Fprintf(w, "%+v\n", err)
+			}
+			fmt.Fprintf(w, "%+v\n", resp.Status)
+			wg.Done()
+		}(url)
+	}
+	wg.Wait()
+}
+
+func main() {
+	http.HandleFunc("/", fetchStatus)
+	http.ListenAndServe(":8082", nil)
+}
