db85d5c006eb51549e5fd871574e3f483c7c3836
raihaninfo <raihanmahmudi35@gmail.com>
2022-02-06 07:54:19 +0600

    send data

--- goChannel/main.go
+++ goChannel/main.go
@@ -9,4 +9,14 @@ func main() {
 		a = make(chan int)
 		fmt.Printf("Type of a is %T\n", a)
 	}
+
+	go func() {
+		for i := 0; i < 100; i++ {
+			a <- i
+		}
+	}()
+
+	for data := range a {
+		fmt.Println(data)
+	}
 }
